e4600caa0b4aaba77ed0741d540dc27959f16378
Matt Kane <m@mk.gg>
2022-01-18 11:16:10 +0000

    chore: use Netlify linting rules

--- src/gatsby-node.js
+++ src/gatsby-node.js
@@ -1,54 +1,74 @@
 // https://www.netlify.com/docs/headers-and-basic-auth/
 
-import WebpackAssetsManifest from "webpack-assets-manifest"
-import { generatePageDataPath } from "gatsby-core-utils"
-import makePluginData from "./plugin-data"
-import buildHeadersProgram from "./build-headers-program"
-import createRedirects from "./create-redirects"
-import {
-  DEFAULT_OPTIONS,
-  BUILD_HTML_STAGE,
-  BUILD_CSS_STAGE,
-  PAGE_COUNT_WARN,
-} from "./constants"
+import { generatePageDataPath } from 'gatsby-core-utils'
+import WebpackAssetsManifest from 'webpack-assets-manifest'
+
+import buildHeadersProgram from './build-headers-program'
+import { DEFAULT_OPTIONS, BUILD_HTML_STAGE, BUILD_CSS_STAGE, PAGE_COUNT_WARN } from './constants'
+import createRedirects from './create-redirects'
+import makePluginData from './plugin-data'
 
 const assetsManifest = {}
 
+/** @type {import("gatsby").GatsbyNode["pluginOptionsSchema"]} */
+export const pluginOptionsSchema = ({ Joi }) => {
+  const MATCH_ALL_KEYS = /^/
+
+  // headers is a specific type used by Netlify: https://www.gatsbyjs.com/plugins/gatsby-plugin-netlify/#headers
+  const headersSchema = Joi.object()
+    .pattern(MATCH_ALL_KEYS, Joi.array().items(Joi.string()))
+    .description(`Add more Netlify headers to specific pages`)
+
+  return Joi.object({
+    headers: headersSchema,
+    allPageHeaders: Joi.array().items(Joi.string()).description(`Add more headers to all the pages`),
+    mergeSecurityHeaders: Joi.boolean().description(`When set to false, turns off the default security headers`),
+    mergeLinkHeaders: Joi.boolean().description(`When set to false, turns off the default gatsby js headers`),
+    mergeCachingHeaders: Joi.boolean().description(`When set to false, turns off the default caching headers`),
+    transformHeaders: Joi.function()
+      .maxArity(2)
+      .description(
+        `Transform function for manipulating headers under each path (e.g.sorting), etc. This should return an object of type: { key: Array<string> }`,
+      ),
+    generateMatchPathRewrites: Joi.boolean().description(
+      `When set to false, turns off automatic creation of redirect rules for client only paths`,
+    ),
+  })
+}
+
 // Inject a webpack plugin to get the file manifests so we can translate all link headers
-exports.onCreateWebpackConfig = ({ actions, stage }) => {
+/** @type {import("gatsby").GatsbyNode["onCreateWebpackConfig"]} */
+
+export const onCreateWebpackConfig = ({ actions, stage }) => {
   if (stage !== BUILD_HTML_STAGE && stage !== BUILD_CSS_STAGE) {
     return
   }
   actions.setWebpackConfig({
     plugins: [
       new WebpackAssetsManifest({
-        assets: assetsManifest, // mutates object with entries
+        // mutates object with entries
+        assets: assetsManifest,
         merge: true,
       }),
     ],
   })
 }
 
-exports.onPostBuild = async (
-  { store, pathPrefix, reporter },
-  userPluginOptions
-) => {
+/** @type {import("gatsby").GatsbyNode["onPostBuild"]} */
+export const onPostBuild = async ({ store, pathPrefix, reporter }, userPluginOptions) => {
   const pluginData = makePluginData(store, assetsManifest, pathPrefix)
   const pluginOptions = { ...DEFAULT_OPTIONS, ...userPluginOptions }
 
   const { redirects, pages } = store.getState()
-  if (
-    pages.size > PAGE_COUNT_WARN &&
-    (pluginOptions.mergeCachingHeaders || pluginOptions.mergeLinkHeaders)
-  ) {
+  if (pages.size > PAGE_COUNT_WARN && (pluginOptions.mergeCachingHeaders || pluginOptions.mergeLinkHeaders)) {
     reporter.warn(
-      `[gatsby-plugin-netlify] Your site has ${pages.size} pages, which means that the generated headers file could become very large. Consider disabling "mergeCachingHeaders" and "mergeLinkHeaders" in your plugin config`
+      `[gatsby-plugin-netlify] Your site has ${pages.size} pages, which means that the generated headers file could become very large. Consider disabling "mergeCachingHeaders" and "mergeLinkHeaders" in your plugin config`,
     )
   }
   reporter.info(`[gatsby-plugin-netlify] Creating SSR redirects...`)
   let count = 0
   const rewrites = []
-  Array.from(pages.values()).forEach(page => {
+  ;[...pages.values()].forEach((page) => {
     const { mode, matchPath, path } = page
     if (mode === `SSR`) {
       count++
@@ -60,61 +80,20 @@ exports.onPostBuild = async (
         {
           fromPath: generatePageDataPath(`/`, matchPath ?? path),
           toPath: `/.netlify/functions/__ssr`,
-        }
+        },
       )
     }
-    if (
-      pluginOptions.generateMatchPathRewrites &&
-      page.matchPath !== page.path
-    ) {
+    if (pluginOptions.generateMatchPathRewrites && matchPath !== path) {
       rewrites.push({
-        fromPath: page.matchPath,
-        toPath: page.path,
+        fromPath: matchPath,
+        toPath: path,
       })
     }
   })
-  reporter.info(
-    `[gatsby-plugin-netlify] Created ${count} SSR redirect${
-      count === 1 ? `` : `s`
-    }...`
-  )
+  reporter.info(`[gatsby-plugin-netlify] Created ${count} SSR redirect${count === 1 ? `` : `s`}...`)
 
   await Promise.all([
     buildHeadersProgram(pluginData, pluginOptions, reporter),
     createRedirects(pluginData, redirects, rewrites),
   ])
 }
-
-const MATCH_ALL_KEYS = /^/
-const pluginOptionsSchema = function ({ Joi }) {
-  // headers is a specific type used by Netlify: https://www.gatsbyjs.com/plugins/gatsby-plugin-netlify/#headers
-  const headersSchema = Joi.object()
-    .pattern(MATCH_ALL_KEYS, Joi.array().items(Joi.string()))
-    .description(`Add more Netlify headers to specific pages`)
-
-  return Joi.object({
-    headers: headersSchema,
-    allPageHeaders: Joi.array()
-      .items(Joi.string())
-      .description(`Add more headers to all the pages`),
-    mergeSecurityHeaders: Joi.boolean().description(
-      `When set to false, turns off the default security headers`
-    ),
-    mergeLinkHeaders: Joi.boolean().description(
-      `When set to false, turns off the default gatsby js headers`
-    ),
-    mergeCachingHeaders: Joi.boolean().description(
-      `When set to false, turns off the default caching headers`
-    ),
-    transformHeaders: Joi.function()
-      .maxArity(2)
-      .description(
-        `Transform function for manipulating headers under each path (e.g.sorting), etc. This should return an object of type: { key: Array<string> }`
-      ),
-    generateMatchPathRewrites: Joi.boolean().description(
-      `When set to false, turns off automatic creation of redirect rules for client only paths`
-    ),
-  })
-}
-
-exports.pluginOptionsSchema = pluginOptionsSchema
