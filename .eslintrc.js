187dff7920b8ec7b121fc318a4c6359e15cb5338
Sarah Etter <sarah.etter@netlify.com>
2022-05-18 16:44:40 -0400

    remove error for unused-vars (#132)

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,7 +12,6 @@ module.exports = {
     'no-param-reassign': 0,
     'no-promise-executor-return': 0,
     'no-prototype-builtins': 0,
-    'no-unused-vars': ['error', { ignoreRestSiblings: true }],
     'unicorn/filename-case': 0,
     'unicorn/numeric-separators-style': 0,
     'unicorn/no-empty-file': 0,
